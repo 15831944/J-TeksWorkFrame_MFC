92dd0db7183877f2623584e2f53a214cb14108f0
±è¿ëÀç <yjkim@unieye.co.kr>
2022-11-17 17:38:52 +0900

    최초 커밋

---
+++ Library/Matrox/MilResult.cpp
@@ -0,0 +1,48 @@
+#include "StdAfx.h"
+#include "MilResult.h"
+#include <Matrox.h>
+
+CMilResult::CMilResult(void)
+{
+	m_milIDResult=M_NULL;
+	m_fModAlloc=FALSE;
+}
+
+CMilResult::~CMilResult(void)
+{
+	if(m_milIDResult)ModFree();
+}
+
+int CMilResult::GetIDResult(void)
+{
+	return (int)m_milIDResult;
+}
+
+int CMilResult::ModAlloc(void)
+{
+	if(!m_milIDResult)
+		MmodAllocResult(CMilSystem::Instance()->GetSystemID(), M_DEFAULT, &m_milIDResult);
+	m_fModAlloc=TRUE;
+	return (int)m_milIDResult;
+}
+
+int CMilResult::ModFree(void)
+{
+	if (!m_milIDResult)return -1;
+	MmodFree(m_milIDResult);
+	m_milIDResult=M_NULL;
+
+	return 0;
+}
+
+int CMilResult::Close(void)
+{
+	if(m_fModAlloc)
+	{
+		ModFree();
+		m_fModAlloc=FALSE;
+	}
+
+	return 0;
+}
+
